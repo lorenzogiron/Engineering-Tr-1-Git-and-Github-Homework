a5d5e283c74431119069f5b5fa6f062f4e6137c2
Lorenzo Giron <lorenzogiron03@gmail.com>
2026-07-27 22:03:28 -0700

    corrected className on image to apply 4px border around it

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="container">
     <h1 className="title">Yosemite National Park</h1>
-    <img className="scenery-img-styled" src="/public/scenery.jpeg" alt="A beautiful scenery" />
+    <img className="scenery-img" src="/public/scenery.jpeg" alt="A beautiful scenery" />
       <p className="description">
         A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail.
       </p>
